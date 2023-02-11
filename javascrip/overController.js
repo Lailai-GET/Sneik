3e38d1616f03d4b49fcda7fe1ef6990cfcff3553
Nikolaipe <nikolaipe@getacademy.no>
2023-02-11 07:03:38 +0100

    removed score.js
    endte med å bare bruke score for over screen, så flyttet til over-filene.
    also din plassering i rank should be bold highlight now

--- javascrip/overController.js
+++ javascrip/overController.js
@@ -1,1 +1,23 @@
 //TODO legge spillers score på highscore list. firebase???????
+function sortHighScores() {
+    let list = model.highscores;
+    return list
+      .sort((a, b) => {
+        if (a.score > b.score) {
+          return 1;
+        }
+        if (a.score < b.score) {
+          return -1;
+        }
+        return 0;
+      })
+      .reverse();
+  }
+  
+  function makeNewScore() {
+    //sender current score til highscore-list
+    model.highscores.push({
+      name: "Din score: ",
+      score: model.score,
+    });
+  }
